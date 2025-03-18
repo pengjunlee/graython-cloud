7cbbdce8b9b42f7004765b4522da374d6d9a14f6
graython <pengjunlee@163.com>
2025-03-18 14:30:59 +0800

    网关直接路由配置

--- demo-common/src/main/java/com/pengjunlee/demo/common/cst/AdminConst.java
+++
@@ -1,8 +0,0 @@
-package com.pengjunlee.demo.common.cst;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class AdminConst {
-
-}
---
+++ user-service/src/main/java/com/pengjunlee/cloud/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.pengjunlee.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
