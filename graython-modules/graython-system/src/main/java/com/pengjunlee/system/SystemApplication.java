7da0c3d72b15f1474a6dff0a2454784b894780a1
pengjunlee <pengjunlee@163.com>
2025-05-28 11:02:12 +0800

    若伊代码提交

--- graython-modules/graython-system/src/main/java/com/pengjunlee/system/SystemApplication.java
+++ graython-modules/graython-system/src/main/java/com/pengjunlee/system/SystemApplication.java
@@ -1,22 +1,11 @@
 package com.pengjunlee.system;
 
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import gray.bingo.db.datasource.DynamicDataSource;
-import gray.bingo.starter.BingoStarter;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.pengjunlee.common.security.annotation.EnableCustomConfig;
 import com.pengjunlee.common.security.annotation.EnableRyFeignClients;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import gray.bingo.starter.BingoStarter;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 
 /**
  * 系统模块
@@ -25,7 +14,7 @@ import org.springframework.core.io.support.ResourcePatternResolver;
  */
 @EnableCustomConfig
 @EnableRyFeignClients
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class },scanBasePackages = {"gray.bingo","com.pengjunlee"})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @MapperScan("com.pengjunlee.system.mapper")  // 指定精确包路径
 public class SystemApplication extends BingoStarter
 {
@@ -33,21 +22,5 @@ public class SystemApplication extends BingoStarter
     {
         run(SystemApplication.class, args);
 
-    }
-
-    @Value("${mybatis.mapper-locations:classpath*:mapper/**/*.xml}")
-    private String mapperLocations;
-
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    @ConditionalOnMissingBean
-    public SqlSessionFactory sqlSessionFactory(DynamicDataSource dynamicDataSource) throws Exception {
-        MybatisSqlSessionFactoryBean sessionFactory = new MybatisSqlSessionFactoryBean();
-        sessionFactory.setDataSource(dynamicDataSource);
-        sessionFactory.setTypeAliasesPackage("com.pengjunlee.**.domain");
-        // 正确加载 XML 文件（支持多级目录）
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sessionFactory.setMapperLocations(resolver.getResources(mapperLocations));
-        return sessionFactory.getObject();
     }
 }
